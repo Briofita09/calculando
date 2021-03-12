feccc2ed769db15e2a1300669d220626b97ac88e
Briofita09 <69055282+Briofita09@users.noreply.github.com>
2021-03-11 23:17:38 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+var botao = document.querySelector("#resultado");
 
-function calculaPreco(gasolina, alcool){
-    var gasolina =  document.getElementById("gasolina").textContent;
-    var alcool = document.getElementById("alcool").textContent;
+function calculaPreco(){
+    var gasolina =  document.getElementById("gasolina").value;
+    var alcool = document.getElementById("alcool").value;
     
-    var melhor = alcool/gasolina;
+    var melhor = alcool / gasolina;
     
     if(melhor > 0.7){
         alert ("GASOLINA");
         }else{ alert ("ÁLCOOL")};
 }      
-
-var botao = document.querySelector("#resultado");
-botao.onclick = calculaPreco();
+botao.addEventListener("click", function (event){
+    event.preventDefault();
+})
